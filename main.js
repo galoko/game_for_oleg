058179ca8982a2d216a3bb486353f4352cd8fa3e
galoko <hootfat@gmail.com>
2019-12-08 04:04:08 +0500

    snowballs

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const vec3 = glMatrix.vec3;
 const vec4 = glMatrix.vec4;
 const mat4 = glMatrix.mat4;
 const toRadian = glMatrix.glMatrix.toRadian;
+const quat = glMatrix.quat;
 
 class GameObjectTemplate {
 	constructor(sprite_id, width, height) {
@@ -13,6 +14,7 @@ class GameObjectTemplate {
 
 const men = new GameObjectTemplate("men", 0.66, 2.0);
 const bush = new GameObjectTemplate("bush", 1.0, 0.58);
+const snowball = new GameObjectTemplate("snowball", 0.15, 0.15);
 
 class GameObject {
 	constructor(template, x, y, z) {
@@ -31,6 +33,9 @@ class GameObject {
 		const z = this.position[2];
 
 		this.topLeft     = vec4.fromValues(x - this.width / 2, y, z - this.height, 1);
+		this.topRight    = vec4.fromValues(x + this.width / 2, y, z - this.height, 1);
+
+		this.bottomLeft  = vec4.fromValues(x - this.width / 2, y, z, 1);
 		this.bottomRight = vec4.fromValues(x + this.width / 2, y, z, 1);
 	}
 }
@@ -43,15 +48,15 @@ const screenHeight = scene.offsetHeight;
 scene.width = screenWidth;
 scene.height = screenHeight;
 
-const cameraPosition = vec3.fromValues(0, 0, -2);
-const cameraLookAt = vec3.fromValues(0, 1, -2);
+const cameraPosition = vec3.fromValues(0, 0, -3.25);
+const cameraLookAt = vec3.fromValues(0, 1, -3);
 
 const projection = mat4.create();
 const view = mat4.create();
 const vp = mat4.create();
 
 const aspectRatio = screenWidth / screenHeight;
-const FOV = 60;
+const FOV = 90;
 mat4.perspective(projection, toRadian(FOV), aspectRatio, 0.1, 1000.0);
 
 function updateView() {
@@ -62,6 +67,35 @@ function updateView() {
 
 updateView();
 
+const output = vec4.create();
+function toScreen(spaceCoord) {
+	vec4.transformMat4(output, spaceCoord, vp);
+
+	return {
+		x: output[0] / output[3],
+		y: output[1] / output[3],
+		z: output[2] / output[3]
+	}
+}
+
+function toCanvas(screenCoord, width, height) {
+	if (width === undefined) {
+		width = screenCoord.width;
+	}
+	if (height === undefined) {
+		height = screenCoord.height;
+	}
+
+	const result = {};
+	result.width  = width  * screenWidth;
+	result.height = height * screenHeight;
+
+	result.x = screenCoord.x * screenWidth  + screenWidth  / 2 - result.width / 2;
+	result.y = screenCoord.y * screenHeight + screenHeight / 2;
+
+	return result;
+}
+
 const objects = [];
 
 objects.push(new GameObject(men,  1, 15, 0));
@@ -70,20 +104,30 @@ objects.push(new GameObject(men,  1.5, 33, 0));
 objects.push(new GameObject(bush, -0.5, 45, 0));
 objects.push(new GameObject(men,  -3, 66, 0));
 
+const snowballs = [];
+
+function randomRange(min, max) {
+	return Math.random() * (max - min + 1) + min;
+}
+
+for (let i = 0; i < 500; i++) {
+	const instance = new GameObject(snowball, randomRange(-3, 3), randomRange(6, 66), -1);
+	instance.original_position = vec3.clone(instance.position);
+	instance.axis = vec3.fromValues(0, 0, 1);
+	vec3.add(instance.position, instance.position, vec3.fromValues(0, 0, -0.7));
+
+	objects.push(instance);
+	snowballs.push(instance);
+}
+
 const player = new GameObject(men, 0, 5, 0);
 objects.push(player);
-
-const topLeftScreen = vec4.create();
-const bottomRightScreen = vec4.create();
-
-const topLeftScreenLeft = vec4.create();
-const topLeftScreenRight = vec4.create();
 
 const groundEnd = vec4.fromValues(0, 1000, 0, 1);
 
 var keys = [];
 
-function draw() {
+function applyControls() {
 	let offsetX = 0.0;
 	let offsetY = 0.0;
 
@@ -93,13 +137,12 @@ function draw() {
 		offsetX = -1;
 	}
 	if (keys[87] || keys[38]) {
-		offsetY =  3;
+		offsetY =  1;
 	} else if (keys[83] || keys[40]) {
-		offsetY = -3;
+		offsetY = -1;
 	}
 	var len = Math.hypot(offsetX, offsetY);
 	if (len > 0) {
-		len = 1;
 		len *= 10;
 
 		offsetX /= len;
@@ -112,22 +155,69 @@ function draw() {
 		vec3.add(cameraLookAt, cameraLookAt, vec3.fromValues(offsetX, offsetY, 0));
 		updateView();
 	}
+}
 
-	context.fillStyle = '#87CEEB';
+function objectToFourPoints(object) {
+	return {
+		topLeft: toScreen(object.topLeft),
+		topRight: toScreen(object.topRight),
+
+		bottomLeft: toScreen(object.bottomLeft),
+		bottomRight: toScreen(object.bottomRight)
+	}
+}
+
+function fourPointsToCenterWidthHeight(fourPoints) {
+	const topWidth    = fourPoints.topRight.x    - fourPoints.topLeft.x;
+	const bottomWidth = fourPoints.bottomRight.x - fourPoints.bottomLeft.x;
+
+	const leftHeight  = fourPoints.bottomLeft.y  - fourPoints.topLeft.y;
+	const rightHeight = fourPoints.bottomRight.y - fourPoints.topRight.y;
+
+	return {
+		x: (fourPoints.topLeft.x + fourPoints.topRight.x + fourPoints.bottomLeft.x + fourPoints.bottomRight.x) / 4,
+		y: (fourPoints.topLeft.y + fourPoints.topRight.y) / 2,
+		z: Math.max(fourPoints.topLeft.z, fourPoints.topRight.z, fourPoints.bottomLeft.z, fourPoints.bottomRight.z),
+		width: (topWidth + bottomWidth) / 2,
+		height: (rightHeight + leftHeight) / 2
+	}
+}
+
+const q = quat.create();
+const delta = vec3.create();
+function flySnowballs() {
+	snowballs.forEach((snowball) => {
+		vec3.sub(delta, snowball.position, snowball.original_position);
+
+		const axisChange = 0.1;
+		vec3.add(snowball.axis, snowball.axis, vec3.fromValues(randomRange(-axisChange, axisChange), randomRange(-axisChange, axisChange), randomRange(-axisChange, axisChange)));
+		vec3.normalize(snowball.axis, snowball.axis);
+
+		quat.setAxisAngle(q, snowball.axis, 0.1);
+
+		vec3.transformQuat(delta, delta, q);
+		vec3.normalize(delta, delta);
+		vec3.add(snowball.position, snowball.original_position, delta);
+
+		snowball.recalcPositions();
+	});
+}
+
+function draw() {
+	applyControls();
+	flySnowballs();
+
+	context.fillStyle = '#87CEEB'; // sky
 	context.fillRect(0, 0, scene.width, scene.height);
 
-	vec4.transformMat4(topLeftScreen, groundEnd, vp);
-	const groundY = topLeftScreen[1] / topLeftScreen[3];
+	const groundPlane = toCanvas(toScreen(groundEnd), 1, 1);
 
 	context.fillStyle = '#9b7653';
-	context.fillRect(0, groundY * screen.height + screenHeight / 2, scene.width, scene.height);
+	context.fillRect(0, groundPlane.y, groundPlane.width, groundPlane.height);
 
 	objects.sort((left, right) => {
-		vec4.transformMat4(topLeftScreenLeft, left.topLeft, vp);
-		vec4.transformMat4(topLeftScreenRight, right.topLeft, vp);
-
-		const zLeft = topLeftScreenLeft[2] / topLeftScreenLeft[3];
-		const zRight = topLeftScreenRight[2] / topLeftScreenRight[3];
+		const zLeft  = fourPointsToCenterWidthHeight(objectToFourPoints(left)).z;
+		const zRight = fourPointsToCenterWidthHeight(objectToFourPoints(right)).z;
 
 		if (zLeft > zRight) 
 			return -1;
@@ -138,26 +228,16 @@ function draw() {
 	});
 
 	objects.forEach((object) => {
-		vec4.transformMat4(topLeftScreen, object.topLeft, vp);
-		vec4.transformMat4(bottomRightScreen, object.bottomRight, vp);
+		const screenRect = fourPointsToCenterWidthHeight(objectToFourPoints(object));
 
-		const z = topLeftScreen[2] / topLeftScreen[3];
+		const z = screenRect.z;
 		if (z > 1) {
 			return;
 		}
 
-		const x1 = topLeftScreen[0] / topLeftScreen[3];
-		const y1 = topLeftScreen[1] / topLeftScreen[3];
+		const canvasRect = toCanvas(screenRect);
 
-		const x2 = bottomRightScreen[0] / bottomRightScreen[3];
-		const y2 = bottomRightScreen[1] / bottomRightScreen[3];
-
-
-
-		const width = x2 - x1;
-		const height = y2 - y1;
-
-		context.drawImage(object.sprite, x1 * screenWidth + screenWidth / 2, y1 * screenHeight + screenHeight / 2, width * screenWidth, height * screenHeight);
+		context.drawImage(object.sprite, canvasRect.x, canvasRect.y, canvasRect.width, canvasRect.height);
 	});
 
 	requestAnimationFrame(draw);
